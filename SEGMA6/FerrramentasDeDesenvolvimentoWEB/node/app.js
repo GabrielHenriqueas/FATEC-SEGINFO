3d214a3cd22b0d87a66ad2eca5b05c0df2c49904
gabrielhenriqueas <gabrielsilva@email.com>
2024-04-19 09:46:47 -0300

    19.04.2024

---
+++ SEGMA6/FerrramentasDeDesenvolvimentoWEB/node/app.js
@@ -0,0 +1,14 @@
+var n1 = 10;
+var n2 = 10;
+
+var total = n1 + n2
+
+console.log("Total: " + total);
+
+if (total <= 10) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+total <= 10 ? console.log("True") : console.log("False");
---
+++ SEGMA6/FerrramentasDeDesenvolvimentoWEB/node/somaApp.js
@@ -0,0 +1,3 @@
+var soma = require("./soma.js")
+
+console.log(soma(20, 22));
