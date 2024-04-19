1ce08b89860adb54f34229395d553249af61c96f
gabrielhenriqueas <gabrielsilva@email.com>
2024-04-19 10:27:10 -0300

    .

--- SEGMA6/FerrramentasDeDesenvolvimentoWEB/node/servidorApp.js
+++ SEGMA6/FerrramentasDeDesenvolvimentoWEB/node/servidorApp.js
@@ -1,7 +1,13 @@
-var http = require('http')
+// var http = require('http')
+import chalk from 'chalk'
+import http from 'http'
+
+var PORT = 8081
 
 http.createServer(function(req, res) {
     res.end("Mensagem direcionada")
-}).listen(8081)
+}).listen(PORT)
 
 console.log("Servidor está ativo!");
+
+console.log(chalk.green(`Porta de conexão: http://localhost:${PORT}`));
